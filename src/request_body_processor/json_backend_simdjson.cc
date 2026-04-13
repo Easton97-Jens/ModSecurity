4d82c2f956927cc425e3dc40ff257f575780aa00
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-04-13 21:54:13 +0200

    Use make_unique for reusable simdjson parser allocation

--- src/request_body_processor/json_backend_simdjson.cc
+++ src/request_body_processor/json_backend_simdjson.cc
@@ -107,12 +107,12 @@ simdjson::ondemand::parser &getReusableSimdjsonParser() {
     if (parser == nullptr) {
 #ifdef MSC_JSON_AUDIT_INSTRUMENTATION
         const auto parser_start = std::chrono::steady_clock::now();
-        parser.reset(new simdjson::ondemand::parser());
+        parser = std::make_unique<simdjson::ondemand::parser>();
         recordSimdjsonParserConstruction(static_cast<std::uint64_t>(
             std::chrono::duration_cast<std::chrono::nanoseconds>(
                 std::chrono::steady_clock::now() - parser_start).count()));
 #else
-        parser.reset(new simdjson::ondemand::parser());
+        parser = std::make_unique<simdjson::ondemand::parser>();
 #endif
     }
     return *parser;
