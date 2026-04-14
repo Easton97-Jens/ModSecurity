669a3bf64fc3d2611c6d70c38c6de755a8fb3a22
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-04-14 18:21:23 +0200

    Add explicit empty defaults for JSON string state members

--- src/request_body_processor/json.h
+++ src/request_body_processor/json.h
@@ -109,8 +109,8 @@ class JSON : public JsonEventSink {
 
     std::deque<std::unique_ptr<JSONContainer>> m_containers;
     Transaction *m_transaction = nullptr;
-    std::string m_current_key;
-    std::string m_data;
+    std::string m_current_key = {};
+    std::string m_data = {};
     double m_max_depth = 0.0;
     int64_t m_current_depth = 0;
     bool m_depth_limit_exceeded = false;
