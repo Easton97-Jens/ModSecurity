f88d4da424c514ceb5929262ad269d4f55254929
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-04-14 17:46:11 +0200

    Drop redundant xml_error empty assignment in XML ctor

--- src/request_body_processor/xml.cc
+++ src/request_body_processor/xml.cc
@@ -142,7 +142,6 @@ XML::XML(Transaction *transaction)
     m_data.doc = nullptr;
     m_data.parsing_ctx = nullptr;
     m_data.sax_handler = nullptr;
-    m_data.xml_error = "";
     m_data.parsing_ctx_arg = nullptr;
     m_data.xml_parser_state = nullptr;
 }
